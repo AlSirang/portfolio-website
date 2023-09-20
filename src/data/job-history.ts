54ae34216828ba7fc687befaddd3269ad7fbf17d
Bashir Uddin <uddinbashir1999@gmail.com>
2023-09-20 14:24:34 +0500

    feat: create job history section

---
+++ src/data/job-history.ts
@@ -0,0 +1,64 @@
+export const jobHistory = [
+  {
+    duration: 'May 2023 — Present',
+    title: 'Co-Founder & CEO',
+    role: 'JavaScript (React.js, Next.js) & Solidity Developer',
+    company: 'E-shepherds Technologies',
+    description:
+      'As CEO and JavaScript developer, I oversee both the management and development of websites, including decentralized platforms. My role involves strategic leadership and hands-on coding to ensure effective web solutions for our projects.',
+    tools: [
+      'JavaScript',
+      'TypeScript',
+      'Solidity',
+      'React.js',
+      'Next.js',
+      'Node.js',
+      'MongoDB',
+      'Redux toolkit',
+      'Tailwind CSS',
+      'ethers.js',
+      'Git',
+      'Hardhat'
+    ]
+  },
+  {
+    duration: 'Feb 2020 — Present',
+    title: null,
+    role: 'Javascript (React.js, Node.js) | Solidity Developer',
+    company: 'Freelance',
+    description:
+      'Applying critical thinking to deconstruct challenges, assess solutions, and make informed decisions was a regular practice. I played a key role in enhancing team communication by preparing comprehensive information for meetings. Constantly acquiring and applying new skills to daily tasks significantly boosted efficiency and productivity. Proficiently managing tight deadlines and a fast-paced environment demonstrated my ability to thrive under pressure.',
+    tools: [
+      'JavaScript',
+      'Solidity',
+      'React.js',
+      'Node.js',
+      'MongoDB',
+      'Redux toolkit',
+      'Tailwind CSS',
+      'Git',
+      'Hardhat'
+    ]
+  },
+  {
+    duration: 'Mar — Dec 2021',
+    title: null,
+    role: 'Javascript (React.js, Node.js) | Solidity Developer',
+    company: 'Webloop',
+    description:
+      'Planned and executed website and decentralized app development, converting mockups into usable web presence using React/Next JavaScript frameworks. Smart Contracts were seamlessly developed and integrated into React/Next.js decentralized apps. Client engagement was a priority for planning and optimizing site issues and queries. Additionally, new mobile-friendly websites were built and styled, facilitating the transition of legacy presentations into easy-to-use, modern versions.',
+    tools: [
+      'JavaScript',
+      'TypeScript',
+      'Solidity',
+      'React.js',
+      'Node.js',
+      'MongoDB',
+      'Redux toolkit',
+      'Material UI',
+      'ethers.js',
+      'Git',
+      'Hardhat'
+    ]
+  }
+]
