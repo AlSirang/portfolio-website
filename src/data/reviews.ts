926a90e063c1d294afb09a365798ebdd6d399759
Bashir Uddin <uddinbashir1999@gmail.com>
2023-09-22 10:38:06 +0500

    fix: update reviews

--- src/data/reviews.ts
+++ src/data/reviews.ts
@@ -4,7 +4,13 @@ export const reviews = [
     country: 'Spain',
     logo: 'S',
     description:
-      'His expertise in React and Web3 demonstrates his ability to work with modern web technologies and integrate blockchain capabilities into applications. This combination of skills can be valuable in developing decentralized applications and implementing blockchain functionality in web projects. Furthermore, Bashir is both professional and honest. He maintains a high level of integrity in his work, ensuring clear communication, timely delivery, and transparency throughout the collaboration. A 5/5 rating professional. Highly recommendable! :)'
+      'I recently hired Bashir as a developer, and I must say that he did an excellent job with my project. I needed a React and Next.js web3 application, and Bashir delivered beyond my expectations. From the very beginning, he showed a deep understanding of the technologies involved and provided valuable insights to enhance the functionality and user experience of the application! :)))'
+  },
+  {
+    username: 'Danorsinai',
+    country: 'Israel',
+    logo: 'D',
+    description: `Bashir's proficiency in React and blockchain technology was evident in the high-quality web app he created for me. The application surpassed my expectations, thanks to Bashir's ability to seamlessly integrate blockchain features, enhancing security and transparency. His technical skills and understanding of the latest industry practices were invaluable throughout the project. Not only is Bashir an expert in his field, but he also possesses excellent communication skills. He effectively conveyed complex concepts in a way that was easy for me to understand, even though I had limited knowledge of the subject matter..`
   },
   {
     username: 'zziomus',
@@ -13,15 +19,15 @@ export const reviews = [
     description: `I'm writing this message just to say Where this guy comes from? The BEST experience that I ever had In my entire life on fiverr. He made my entire project, without even placing the order. Was working like crazy to make my project. Very responsive. Bashir even ask for a single penny, like most of the seller's on fiver. He's entire focus was to made my project. Great job.`
   },
   {
+    username: 'Boostedaj',
+    country: 'United States',
+    logo: 'B',
+    description: `Bashir was great to work with, very easy to talk to, and went above and beyond to make sure I had everything I needed for my project. He even put in the time to do extra research into the area of expertise for my smart contract. Thank you!`
+  },
+  {
     username: 'Phipbc',
     country: 'Turkey',
     logo: 'P',
     description: `Our experience with Bashir was Top Notch! We required Web3 integration into our website and we couldn't have picked a better seller to work with! He's genuinely interested in the satisfaction and success of his clients projects and goes the extra mile to find solutions to solve our needs specific needs! Will definitely use him again, Highly recommended!`
-  },
-  {
-    username: 'Zuko0404',
-    country: 'Australia',
-    logo: 'Z',
-    description: `I found Mr Bashir very professional with integrity. He delivered as promised. The job took a lot shorter than I was expecting it to be. Great job Mr Bashir & I wish you the best. Cheers.`
   }
 ]
